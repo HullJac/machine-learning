5c1d71af9c8f6b9dbee160ba543bb4daed843961
HullJac <jacobhull55@gmail.com>
2021-10-20 12:44:11 -0500

    adding class pip file and new code from today

--- class/softmaxSimplix.py
+++ class/softmaxSimplix.py
@@ -46,11 +46,11 @@ def plot_learning_curves(model,X,y,mx,my):
 
 
 rawData=pd.read_csv('iris.csv')
-data = rawData.replace(to_replace=['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'], value=[0,1,2])
+dataR = rawData.replace(to_replace=['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'], value=[0,1,2])
 
-print(data)
+print(dataR)
 
-data = data.to_numpy()
+data = dataR.to_numpy()
 
 SL = data[:,1]
 SW = data[:,2]
@@ -102,7 +102,7 @@ def cost(o,t):
 
 
 # take the pedal information
-x = data[:,3:5]
+x = copy.deepcopy(data[:,3:5])
 #y is defined above
 
 # normalize based on the variation in the data
@@ -170,4 +170,128 @@ for i in range(n):
 
 fig = plt.figure()
 plt.plot(costy)
+plt.show()
+
+x0set = x[:,1]
+x1set = x[:,2]
+
+x0min = np.amin(x0set)
+x0max = np.amax(x0set)
+
+x1min = np.amin(x1set)
+x1max = np.amax(x1set)
+
+delx0 = (x0max - x0min)/40.
+delx1 = (x1max - x1min)/40.
+
+# create an array of each so we can get a serries of samples
+x0 = np.zeros(40)
+x1 = np.zeros(40)
+
+pair = np.empty((1600,3), float)
+
+for i in range(40):
+    x0[i] = x0min + i * delx0
+    x1[i] = x1min + i * delx1
+
+k = 0
+for i in range(40):
+    for j in range(40):
+        pair[k][0] = 1
+        pair[k][1] = x0[i]
+        pair[k][2] = x1[j]
+        k += 1
+
+
+s = softmax(pair.dot(w))
+
+yguess = to_class(s)
+
+fig = plt.figure()
+plt.scatter(pair[:,1], pair[:,2], c=yguess, cmap=colormap, alpha = 0.2)
+plt.show()
+
+# Normalize pedal data
+PL[:] = (PL[:]-PL[:].mean())/PL[:].std()
+PW[:] = (PW[:]-PW[:].mean())/PW[:].std()
+
+plt.scatter(PL, PW, c=color_indices, cmap = colormap)
+plt.show()
+
+
+#############################################
+# it is sciklearn brain on neutral time
+# this is doing a whoel lot fo things
+# we are doing a softmax linear regression
+
+data = dataR.to_numpy()
+x = copy.deepcopy(data[:,3:5])
+
+# we will use the industry standard scaler normalization
+# subtract mean and divide by unit variate deviation // only slightly differnt as it is normalized to 1
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+# first normalize
+scaler = StandardScaler()
+scaler.fit(x)
+x = scaler.transform(x)
+
+# do not add a column of ones for this because they do it internally for us
+
+softmax_sci = LogisticRegression(multi_class="multinomial", solver="lbfgs",C=10) # well come back to this
+# C here is like the alpha
+
+softmax_sci.fit(x,y)
+
+# try to test it out
+xx = [[5,1]] # this is the value we want to predict
+
+px = scaler.transform(xx)
+
+print(softmax_sci.predict(px))
+print(softmax_sci.predict_proba(px))
+
+# Graphing
+x0set = x[:,0]
+x1set = x[:,1]
+
+x0min = np.amin(x0set)
+x0max = np.amax(x0set)
+
+x1min = np.amin(x1set)
+x1max = np.amax(x1set)
+
+delx0 = (x0max - x0min)/40.
+delx1 = (x1max - x1min)/40.
+
+# create an array of each so we can get a serries of samples
+x0 = np.zeros(40)
+x1 = np.zeros(40)
+
+pair = np.empty((1600,2), float)
+
+for i in range(40):
+    x0[i] = x0min + i * delx0
+    x1[i] = x1min + i * delx1
+
+k = 0
+for i in range(40):
+    for j in range(40):
+        pair[k][0] = x0[i]
+        pair[k][1] = x1[j]
+        k += 1
+
+yguess = softmax_sci.predict(pair)
+
+fig = plt.figure()
+plt.scatter(pair[:,0], pair[:,1], c=yguess, cmap=colormap, alpha = 0.2)
+plt.show()
+sys.exit()
+
+# Normalize pedal data
+PL[:] = (PL[:]-PL[:].mean())/PL[:].std()
+PW[:] = (PW[:]-PW[:].mean())/PW[:].std()
+
+plt.scatter(PL, PW, c=color_indices, cmap = colormap)
 plt.show()
