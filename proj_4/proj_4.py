77a4c45c553da1b8fabea1b8d40bef34620eecbd
HullJac <jacobhull55@gmail.com>
2021-11-18 19:15:08 -0600

    got 90 percent on test on different data

--- proj_4/proj_4.py
+++ proj_4/proj_4.py
@@ -26,31 +26,17 @@ from sklearn.ensemble import RandomForestClassifier
 # Grab the data
 rawData = pd.read_csv('StockTrainBin.csv')
 
-# Add a new column of data dn print to make sure it is there
-#rawData["BST"] = np.sqrt(pow(rawData["DCP"], 2) + pow(rawData["CFP"], 2) + pow(rawData["VP"], 2))
-#print(rawData)
-
 # Convert to numpy 
 data = rawData.to_numpy()
-# Separate the data and set aside some for testing
+
+# Set some data aside some for extra testing and split into x and y
 testData = data[:-30:,:]
-#testX1 = testData[:,1:4]
-#testX2 = testData[:,6:]
-#testX = np.concatenate((testX1,testX2), axis=1)
 testX = testData[:,1:]
 testY = testData[:,0]
 
-
-#x1 = data[:-30,1:4]
-#x2 = data[:-30,6:]
-#X = np.concatenate((x1,x2), axis=1)
-#print(X)
+# Separate the x and y 
 X = data[:-30,1:]
 y = data[:-30,0]
-
-# Polynomialize the data 
-#poly = PolynomialFeatures(degree=1, include_bias=False)
-#X = poly.fit_transform(X)
 
 # Scale all the data
 scaler = StandardScaler()
@@ -63,70 +49,50 @@ svm_clf = SVC( # SVM
         C=5,
         kernel='poly',
         coef0=1,
-        degree=6, #5-8 got 86
+        degree=6,
         probability=True,
         gamma="auto",
         max_iter=50000000)
 soft_clf = LogisticRegression( # SoftMmax
         multi_class="multinomial",
         solver="lbfgs",
-        C=15,  # C was 30 before
+        C=15,
         max_iter=50000000)
 rnd_clf = RandomForestClassifier( # RandomForest
         n_estimators=2000,
         bootstrap=True,
-        max_samples=0.7, # 0.7 did better thatn 0.5
+        max_samples=1.0,
         max_features=7,
         max_leaf_nodes=5000,
         max_depth=5000,
         n_jobs=-1)
 
 
-# Create the classifier
+# Create the voting classifier
 stock_clf = VotingClassifier(
         estimators=[('rf', rnd_clf),  ('sm', soft_clf), ('sv', svm_clf)],
         voting='soft')
 
-for i in range(7):
-    # Test the classifiers
-    for clf in (rnd_clf, soft_clf, svm_clf, stock_clf):
-        # Splitting the data
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-        # Fitting the models and predicting
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-        print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
+# Test the classifiers
+for clf in (rnd_clf, soft_clf, svm_clf, stock_clf):
+    # Splitting the data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+    # Fitting the models and predicting
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
 
-    # Test on data not in the set
-    y_pred = stock_clf.predict(testX)
-    acc = accuracy_score(testY, y_pred)
-    print("Test on new data: " + str(acc))
-    print("------------------ "+str(i+1)+" ---------------------")
+# Test on data not in the set
+y_pred = stock_clf.predict(testX)
+acc = accuracy_score(testY, y_pred)
+print("Test on new data: " + str(acc))
 
 
-'''
-bag_clf = BaggingClassifier(
-        DecisionTreeClassifier(),
-        n_estimators=5000, # number of decision trees to train
-        max_samples=0.4, # higher number here decreases variance, but increases bias
-        bootstrap=False, # if this is false, it would do pasting And maks it so no repeats are chosen forthe specific node at focus right now  #########true and false do about the same
-        n_jobs=-1,
-        random_state=42,
-        max_features=7,
-        bootstrap_features=True,
-        #oob_score=True # if you want to use this, you do not have to use the 80/20 split for validataion, this is the validation
-)
-
-bag_clf.fit(X_train, y_train)
-
-y_pred = bag_clf.predict(X_test)
-
-print(accuracy_score(y_test, y_pred))
-#print(bag_clf.oob_score_)
-
-'''
+# The code below was used to check out the importance of the features
+# to try and find some that may not be as important or some that are
+# more important than others
 '''
 ##########Random Forests###########
 # same thing as bagging with decision trees
